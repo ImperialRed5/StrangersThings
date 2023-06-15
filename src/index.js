8a13006586cfd207bdddc76ff90e87481488ea5c
ImperialRed5 <rosay_21@icloud.com>
2023-06-15 18:54:16 -0400

    more stuff

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 import { fetchPosts } from "./api/index";
 
 const App = () => {
-  const [posts, setPosts] = useState("");
-  const fetchResults = fetchPosts();
-  console.log(fetchResults);
-  return <div></div>;
-};
+  const [posts, setPosts] = useState([]);
 
-// fetchPosts();
+  useEffect(() => {
+    const fetchResults = async () => {
+      const response = await fetchPosts();
+      setPosts(response.data.posts);
+    };
+    fetchResults();
+  }, []);
+
+  return (
+    <div>
+      <h1>Stranger's Things</h1>
+      <h2>Posts</h2>
+      {posts &&
+        posts.map((post) => {
+          return (
+            <div key={post._id}>
+              <h3>{post.title}</h3>
+              <p>{post.description}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
